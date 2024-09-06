cbb148d04ec95a55a0d7f1901dc7ec9bd10497d6
Rezija <144447054+rezichu@users.noreply.github.com>
2024-09-06 09:41:21 +0300

    uzd2.py

--- uzd2.py
+++ uzd2.py
@@ -9,9 +9,12 @@ def pievenot_masinu(nosaukums, marka, gads):
     print(f"Mašīna '{nosaukums}' pievienota sarakstam")
 
 def paradit_masinas():
-    for masina in masinas:
-        print(f"Nosaukums: {masina['nosaukums']}, Marka: {masina['marka']}, Gads: {masina['gads']}")
-    #???
+    if masinas:
+        print('\nMašīnas sarakstā: ')
+        for masina in masinas:
+            print(f"Nosaukums: {masina['nosaukums']}, Marka: {masina['marka']}, Gads: {masina['gads']}")
+    else:
+        print('\nSaraksts ir tukšs.')
 
 def atjaunot_masinu(nosaukums, jauna_marka, jauns_gads):
     for masina in masinas:
@@ -22,3 +25,46 @@ def atjaunot_masinu(nosaukums, jauna_marka, jauns_gads):
             return
     print(f"Mašīna '{nosaukums}' nav atrasta sarakstā.")
     #progress 4.septembrī, turpināt 6.septembrī
+
+def dzest_masinu(nosaukums):
+    for masina in masinas:
+        if masina['nosaukums'] == nosaukums:
+            masinas.remove(masina)
+            print(f"Mašīna '{nosaukums}' dzēsta no saraksta.")
+            return
+    print(f"Mašīna '{nosaukums}' nav atrasta sarakstā.")
+
+while True:
+    print('\nIzvēlies darbību:')
+    print('1. Pievienot mašīnu')
+    print('2. Parādīt visas mašīnas')
+    print('3. Atjaunināt mašīnu')
+    print('4. Dzēst mašīnu')
+    print('5. Iziet')
+
+    izvele = input('Izvēlies darbību(1-5): ')
+    if izvele == '1':
+        nosaukums = input('Ievadiet mašīnas nosaukumu: ')
+        marka = input('Ievadiet mašīnas marku: ')
+        gads = input('Ievadiet mašīnas gadu: ')
+        pievenot_masinu(nosaukums, marka, gads)
+
+    elif izvele == '2':
+        paradit_masinas()
+
+    elif izvele == '3':
+        nosaukums = input('Ievadiet nosaukumu, kuru vēlaties atjaunot: ')
+        jauna_marka = input('Ievadiet jaunu marku: ')
+        jauns_gads = input('Ievadiet jauno mašīnas gadu: ')
+        atjaunot_masinu(nosaukums, jauna_marka, jauns_gads)
+    
+    elif izvele == '4':
+        nosaukums = input('Ievadiet nosaukumu mašīnai, ko vēlaties dzēst no saraksta: ')
+        dzest_masinu(nosaukums)
+
+    elif izvele == '5':
+        print('Programma beidzas. Atā! ♪(´▽｀)')
+        break
+
+    else:
+        print('Nepareiza ievade.')
